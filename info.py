c8b8ee536faeaa960aa6d7a030fa7827ac2b8947
Role417 <157006183+Role417@users.noreply.github.com>
2024-09-19 00:08:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ REQ_CHANNEL = (int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '--1002276401285'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002276401285'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MOVIES_ZILAA')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'False')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), False)
